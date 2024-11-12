13d08780489ecc82f8ea39e181c14b096f73d290
Caio Pereira <caiorochapstrlrd@gmail.com>
2024-11-12 15:09:43 -0300

    add neotree toggle

--- init.lua
+++ init.lua
@@ -83,6 +83,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('n', '<leader>db', vim.cmd.DBUI)
+vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
